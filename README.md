4a06c82496471ac7dd7a7e4476c2091f72749c71
Carlos Salas <csalaseiroa@correo.ugr.es>
2021-04-12 23:23:58 +0200

    release version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # passwordGenerator
 simple password generator implemented with python
+
+## Usage
+
+Help message:
+```
+$python3 passwordGen.py -h
+$python3 passwordGen.py --help
+```
+
+Ececution:
+```
+$python3 passwordGen.py -l <password length>
+```
---
+++ passwordGen.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+# Made by: @Solracs
+
+import sys
+import secrets
+import argparse
+
+# list of all printable chars
+printable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+password = ""
+
+
+#setting args parser up
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-l', type = int, required = True, help = "passwod length")
+
+parser.parse_args()
+
+args = parser.parse_args()
+
+if args.l:
+
+    try:
+        length = args.l
+
+        for i in range(length):
+            password += secrets.choice(printable)
+
+        print("Password generated:")
+        print(password)
+
+    except:
+        print("\nInvalid argument... length must be an integer\n")
+
